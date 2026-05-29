a36e88d934ffc778283fc77fc3a6820a152c83c2
Lavanya Gali <lavanyagali1272@gmail.com>
2026-05-29 12:05:33 +0530

    TDS-101: backend wallet and transaction updates

--- tmp/test_db.ts
+++ tmp/test_db.ts
@@ -1,5 +1,5 @@
-import { db } from './src/db';
-import { users, wallets, transactions } from './src/db/schema';
+import { db } from '../src/db';
+import { users, wallets, transactions } from '../src/db/schema';
 import { eq } from 'drizzle-orm';
 
 async function testInsert() {
